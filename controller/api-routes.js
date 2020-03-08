021f5c3a749b4f5ffb0d437b4aa0fb0038e1a213
Kirill V <kirilliuswest@gmail.com>
2020-03-07 20:15:34 -0500

    completed routes to save book and get all books

--- controller/api-routes.js
+++ controller/api-routes.js
@@ -6,6 +6,18 @@ const apiRoutes = express.Router();
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/googlebooks", { useNewUrlParser: true });
 
+apiRoutes.get('/api/books', async function(req, res) {
+    let allBooks = await Book.find();
+    //console.log(allBooks);
+    res.json(allBooks);
+});
 
+apiRoutes.post('/api/books', function(req, res) {
+    let book = new Book(req.body);
+    Book.create(book)
+        .then(dbBook => res.status(200).send(dbBook)
+    )
+        .catch(err => {res.json(err);});
+});
 
 module.exports = apiRoutes;
--- database/bookModel.js
+++ database/bookModel.js
@@ -3,7 +3,27 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const BookSchema = new Schema({
-    
+    title: {
+        type: String,
+        required: 'Book title is required'
+    },
+    author: {
+        type: Array,
+        required: 'Book author is required'
+    },
+    description: {
+        type: String
+    },
+    image: {
+        type: String
+    },
+    link: {
+        type: String
+    },
+    dateAdded: {
+        type: Date,
+        default: Date.now
+    }
 });
 
 const Book = mongoose.model("Book", BookSchema);
