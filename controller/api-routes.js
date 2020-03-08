00412b883e085436a26a4359c467aa0a62764605
Kirill V <kirilliuswest@gmail.com>
2020-03-08 15:32:41 -0400

    completed api route and external axios route for searching google books

--- controller/api-routes.js
+++ controller/api-routes.js
@@ -1,10 +1,17 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const Book = require("../database/bookModel");
+const axios = require("axios");
+const fs = require("fs");
 
 const apiRoutes = express.Router();
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/googlebooks", { useNewUrlParser: true });
+
+function getKey(){
+    let key =  fs.readFileSync("C:/Users/Kirill/projects/utils/GoogleAPI/APIKey.txt","utf8");
+    return key;
+}
 
 apiRoutes.get('/api/books', async function(req, res) {
     let allBooks = await Book.find();
@@ -18,6 +25,29 @@ apiRoutes.post('/api/books', function(req, res) {
         .then(dbBook => res.status(200).send(dbBook)
     )
         .catch(err => {res.json(err);});
+});
+
+apiRoutes.delete('/api/books/:id', function(req, res) {
+    let bookId = req.params.id;
+    Book.deleteOne({_id:bookId})
+        .then(dbBook => res.status(200).send('Deleted successfully')
+    )
+        .catch(err => {res.json(err);});
+});
+
+apiRoutes.get('/api/books/search/:term', async function(req, res) {
+    let term = req.params.term;
+    let key;
+    if (process.env.GOOG_API) {
+        key = process.env.GOOG_API;
+    } else {
+        key = getKey();
+    }
+    let apiStr = `https://www.googleapis.com/books/v1/volumes?q=${term}&key=${key}`;
+    let data = await axios.get(apiStr);
+    //console.log('data: ', data);
+    let books = data.data.items;
+    res.send(books);
 });
 
 module.exports = apiRoutes;
